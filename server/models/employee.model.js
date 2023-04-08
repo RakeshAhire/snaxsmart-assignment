04b6de91547e3f6fa22d58946839778d4e3f2a78
RakeshAhire <rocky5509ahire@gmail.com>
2023-04-08 16:27:32 +0530

    update

--- server/models/employee.model.js
+++ server/models/employee.model.js
@@ -10,7 +10,7 @@ const employeeSchema = new Schema({
         required:true
     },
     cardNo: {
-        type: Number,
+        type: String,
         required:true
     },
     email: {
@@ -19,7 +19,7 @@ const employeeSchema = new Schema({
         unique:true
     },
     contactNo: {
-        type: Number,
+        type: String,
         required:true
     },
     department: {
